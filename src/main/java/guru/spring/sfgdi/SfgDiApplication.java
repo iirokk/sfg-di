5a198af575cd51d3f7f8d837e7f946b4121df3a0
Iiro Kokkonen <iiro.kokkonen@cinia.fi>
2021-01-08 15:15:39 +0200

    Spring profiles to select injected bean (language profiles).

--- src/main/java/guru/spring/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/spring/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.spring.sfgdi;
 
-import guru.spring.sfgdi.controllers.ConstructorInjectedController;
-import guru.spring.sfgdi.controllers.MyController;
-import guru.spring.sfgdi.controllers.PropertyInjectedController;
-import guru.spring.sfgdi.controllers.SetterInjectedController;
+import guru.spring.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) context.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) context.getBean("myController");
 		System.out.println("------------ Primary");
