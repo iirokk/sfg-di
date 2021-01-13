a0eb1813cf5d127874530a4492af9da8943a4f9e
Iiro Kokkonen <iiro.kokkonen@cinia.fi>
2021-01-13 10:47:13 +0200

    Properties from 2 files.

--- src/main/java/guru/spring/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/spring/sfgdi/SfgDiApplication.java
@@ -2,6 +2,7 @@ package guru.spring.sfgdi;
 
 import guru.spring.sfgdi.controllers.*;
 import guru.spring.sfgdi.examplebeans.PlaceholderDataSource;
+import guru.spring.sfgdi.examplebeans.PlaceholderMQBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +16,12 @@ public class SfgDiApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		PlaceholderDataSource dataSource = (PlaceholderDataSource) ctx.getBean(PlaceholderDataSource.class);
+		PlaceholderDataSource dataSource = ctx.getBean(PlaceholderDataSource.class);
 		System.out.println(dataSource.getUser());
 		System.out.println(dataSource.getLocale());
+
+		PlaceholderMQBroker mqBroker = ctx.getBean(PlaceholderMQBroker.class);
+		System.out.println(mqBroker.getUrl());
 	}
 
 }
--- src/main/java/guru/spring/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/spring/sfgdi/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package guru.spring.sfgdi.config;
 
 import guru.spring.sfgdi.examplebeans.PlaceholderDataSource;
+import guru.spring.sfgdi.examplebeans.PlaceholderMQBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:mq.properties")
+})
+
 public class PropertyConfig {
 
     @Autowired
@@ -23,6 +29,13 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     String dburl;
 
+    @Value("${guru.mq.username}")
+    String mqUser;
+    @Value("${guru.mq.password}")
+    String mqPassword;
+    @Value("${guru.mq.url}")
+    String mqUrl;
+
     @Bean
     public PlaceholderDataSource placeholderDataSource() {
         PlaceholderDataSource dataSource = new PlaceholderDataSource();
@@ -31,6 +44,15 @@ public class PropertyConfig {
         dataSource.setDburl(dburl);
         dataSource.setLocale(environment.getProperty("LANG"));
         return dataSource;
+    }
+
+    @Bean
+    public PlaceholderMQBroker placeholderMQBroker() {
+        PlaceholderMQBroker placeholderMQBroker = new PlaceholderMQBroker();
+        placeholderMQBroker.setUser(mqUser);
+        placeholderMQBroker.setPassword(mqPassword);
+        placeholderMQBroker.setUrl(mqUrl);
+        return placeholderMQBroker;
     }
 
     @Bean
